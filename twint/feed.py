b8d714159b1ca32c5ed9e60cf0d356ed7936f049
Mateusz Gawel <mrbalu9@gmail.com>
2019-03-24 21:43:36 +0100

    revert an old version of json method

--- twint/feed.py
+++ twint/feed.py
@@ -40,14 +40,10 @@ def profile(response):
 def Json(response):
     logme.debug(__name__+':Json')
     json_response = loads(response)
-
     html = json_response["items_html"]
     soup = BeautifulSoup(html, "html.parser")
-
     feed = soup.find_all("div", "tweet")
-    min_position = json_response["min_position"]
-
-    return feed, min_position
+    return feed, json_response["min_position"]
 
 def comments(response, last_position=None):
     logme.debug(__name__+':comments')
